4536740c846ca41cd4fcd6f733b50a9099f1ddbc
jllord <jessica@codeforamerica.org>
2012-08-11 18:56:59 -0700

    turned scroolwheelzoom off of map

--- sheetsee.js
+++ sheetsee.js
@@ -112,7 +112,9 @@ function displayAddress(map, project) {
 }
 
 function loadMap() {
-  var	map = new L.Map('map');
+  var	map = new L.Map('map', {
+    scrollWheelZoom: false,
+    dragging: true});
 	var cloudmade = new L.TileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
 	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
 	    maxZoom: 18
