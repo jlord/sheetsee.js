5adadb6ed6c9c4c3b0cfd3518195f4bc86a1455e
jlord <jessica.lord@github.com>
2013-10-19 19:43:41 -0700

    fix cdn links, edit build components

--- js/sheetsee.full.js
+++ js/sheetsee.full.js
@@ -4,6 +4,6 @@ var extend   = require('lodash.assign');
 // consider this a "build file!" you can add/remove dependencies here, and the file will be created at dist/sheetsee.full.js.
 // creating these components are easy, just export an object, and things inside it will be extended to the main Sheetsee here.
 // in the future, these could be separate node modules. (for example: sheetsee-d3, sheetsee-mapbox, etc)
-extend(Sheetsee, require('./components/d3'), require('./components/mapbox'))
+extend(Sheetsee, require('./components/sheetsee-charts'), require('./components/sheetsee-maps'), require('./components/sheetsee-tables'))
 
 module.exports = Sheetsee;
