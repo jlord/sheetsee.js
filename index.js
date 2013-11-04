6cbb4a8c14e5ed999b7de59e8cfd423ce3505144
jlord <jessica.lord@github.com>
2013-11-04 09:39:46 +0000

    modularize

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var Sheetsee = require('./sheetsee')
-var extend   = require('lodash.assign');
+var Sheetsee = {}
+var extend   = require('lodash.assign')
 
 // consider this a "build file!" you can add/remove dependencies here, and the file will be created at dist/sheetsee.full.js.
 // creating these components are easy, just export an object, and things inside it will be extended to the main Sheetsee here.
 // in the future, these could be separate node modules. (for example: sheetsee-d3, sheetsee-mapbox, etc)
-extend(Sheetsee, require('./components/sheetsee-charts'), require('./components/sheetsee-maps'), require('./components/sheetsee-tables'))
+extend(Sheetsee, require('sheetsee-core'), require('sheetsee-tables'), require('sheetsee-maps'), require('sheetsee-charts'))
 
-module.exports = Sheetsee;
+module.exports = Sheetsee
--- makefile
+++ makefile
@@ -1,7 +1,5 @@
 build:
-	browserify js/sheetsee.js -s Sheetsee > dist/sheetsee.js
-	browserify js/sheetsee.full.js -s Sheetsee > dist/sheetsee.full.js
+	browserify index.js -s Sheetsee > dist/sheetsee.js
 	uglifyjs dist/sheetsee.js > dist/sheetsee.min.js
-	uglifyjs dist/sheetsee.full.js > dist/sheetsee.full.min.js
 	
 .PHONY: build
